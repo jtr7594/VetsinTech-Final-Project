e93fe7802912e3dc4843f571e054b397cd6ba162
Jeff Ryals <sprocket_44t@yahoo.com>
2019-08-20 19:55:48 -0700

    added 2 more html pages, mouse over/leave functionality

---
+++ cycling.js
@@ -0,0 +1,9 @@
+var cyclingOne = document.getElementById("cyclingOne");
+
+function onMouseOver(){
+cyclingOne.style.color = "red"
+}
+
+function onMouseLeave(){
+cyclingOne.style.color = "black"
+}
